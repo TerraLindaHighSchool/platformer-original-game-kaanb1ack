3ed7920947facdc452846a80e83588aa0d7a197e
kaanb1ack <89489424+kaanb1ack@users.noreply.github.com>
2021-09-23 15:42:41 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -8,12 +8,45 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Player extends Actor
 {
-    /**
-     * Act - do whatever the Player wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private Health[] health;
+    private Powerup[] powerup;
+    private int healthCount;
+    private int speed;
+    private int walkIndex;
+    private int frame;
+    private float yVelocity;
+    private boolean isWalking;
+    private boolean isJumping;
+    private boolean isFacingLeft;
+    private GreenfootImage[] WALK_ANIMATION;
+    private GreenfootImage STANDING;
+    private float JUMP_FORCE;
+    private float GRAVITY;
+    private Class NEXT_LEVEL;
+    private GreenfootSound MUSIC;
+    
+    public Player(int speed, int jumpForce, float gravity, int maxHealth, int maxPowerup, Class nextLevel, GreenfootSound music)
+    {
+    
+    }
+    
     public void act()
     {
         // Add your action code here.
     }
+    
+    public void addedToWorld(World world) {}
+    
+    private void walk() {}
+    private void jump() {}
+    private void fall() {}
+    private void animator() {}
+    private void onCollsion() {}
+    private void mirrorImages() {}
+    private void gameOver() {}
+    private boolean isOnGround() 
+    {
+        return false;
+    }
+    
 }
